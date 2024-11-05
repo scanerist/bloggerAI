397cc275914c1e4fb7c88874bdfa476994e9083a
Alexei Mironov <alexeidotpy@gmail.com>
2024-11-05 17:28:25 +0600

    Fix missed media messages, add log.info

--- services/bot/bot_utils.py
+++ services/bot/bot_utils.py
@@ -1,19 +1,27 @@
 import asyncio
-from typing import Optional
+from typing import Optional, Any
 
 from aiogram import types
 from aiogram.fsm.context import FSMContext
+from pyrogram.errors import exceptions
+
 
 from services.bot.keyboards import post_approval_keyboard
-from services.openai_service.openai_client import OpenAIService
+#from services.openai_service.openai_client import OpenAIService
 from services.pyrogram_service.pyrogram_client import PyrogramService
 from services.bot.state_manager import Form
 from services.database.dao import update_source_channel
 from services.database.dao import get_message_id_from_source_channel_by_user_id
 
-openai_service = OpenAIService()
+from services.shared.logger import setup_logger
+
+logger = setup_logger(__name__)
+#openai_service = OpenAIService()
 pyrogram_service = PyrogramService.get_instance()
 
+
+def is_content(message: types.Message):
+    return 
 
 async def process_next_post(message: types.Message, state: FSMContext, source_channel: Optional[str] = None):
     data = await state.get_data()
@@ -23,16 +31,40 @@ async def process_next_post(message: types.Message, state: FSMContext, source_ch
         return
 
     instruction = data.get('instruction')
-    last_message = await pyrogram_service.get_last_message(source_channel)
-    content: str = last_message.text or last_message.caption or ""
-    if last_message.id == data.get('last_processed_message_id'):
-        await message.answer("Все посты обработаны. Пропуск...")
+    try:
+        last_message = await pyrogram_service.get_last_message(source_channel)
+    except exceptions.UsernameInvalid as e:
+        err_message = f"Не поддерживаемый формат имени пользователя {source_channel}"
+        logger.warning(err_message)
+        raise ValueError(err_message)
+    content: Any = last_message.text or last_message.caption or last_message.media or ""
+    last_processed_message_id = data.get('last_processed_message_id')
+    if last_message.id == last_processed_message_id:
+        await message.answer(
+            f"Все посты обработаны. Пропуск..."\
+            f"Детали: из канала: {source_channel}, last_message_id: {last_message.id}, "\
+            f"last processed message_id, записанное в state.data: {last_processed_message_id}"\
+            f"message_id на вход метода `process_next_post` message.id {message.message_id}"
+        )
         return
     if not content:
-        await message.answer("Пустой пост. Пропуск...")
-        await process_next_post(message, state)
-        return
-    modified_content = await openai_service.modify_post(content, instruction)
+        await message.answer(
+            f"Пустой пост. Пропуск... "\
+            f"Детали: из канала: {source_channel}, last_message_id: {last_message.id}, "\
+            f"last processed message_id, записанное в state.data: {last_processed_message_id}"\
+            f"message_id на вход метода `process_next_post` message.id {message.message_id}"
+        )
+        try:
+            await process_next_post(message, state)
+            return
+        except RecursionError as e:
+            await message.answer(f"Проблема c обработкой сообщений из этого канала {source_channel}")
+            return
+    #modified_content = await openai_service.modify_post(content, instruction)
+    modified_content = content +  "МОДИФИЦИРОВАН!" if type(content) == str else content
+
+    if type(modified_content) != str:
+        logger.info(f"Отправляемый контент не строка, а нечто: {modified_content}, вот все его атрибуты: {dir(modified_content)}")
 
     await state.update_data(modified_content=modified_content)
     await update_source_channel(source_channel, last_message.id)
@@ -63,10 +95,18 @@ async def monitor_channel_and_notify(message: types.Message, state: FSMContext, 
         # Получаем ID последнего обработанного сообщения
         last_processed_message_id = await get_message_id_from_source_channel_by_user_id(source_channel, user_id)
 
+        logger.info(f"last message id is: {last_message.id}, last processed message id is: {last_processed_message_id}, message_id: {message.message_id}")
         # Если есть новое сообщение, обрабатываем его
         if last_message.id > last_processed_message_id:
             await process_next_post(message, state, source_channel)
-            print(last_message.id, last_processed_message_id, message.message_id, source_channel, destination_channel)
+            logger.info(
+                "Pyrogram обнаружил новое сообщение"\
+                f"last message id is: {last_message.id},"\
+                f"last processed message id is: {last_processed_message_id},"\
+                f"message_id (from message at aiogram's input): {message.message_id},"\
+                f"source_channel: {source_channel},"\
+                f"destination_channel: {destination_channel}"
+            )
 
             await message.answer(f"Новое сообщение обработано и отправлено в {destination_channel}")
 
