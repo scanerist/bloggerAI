d96dfebaf4090faeb5b55beba13bee534251b309
Alexei Mironov <alexeidotpy@gmail.com>
2024-12-20 22:51:33 +0600

    Add a filestream logger

--- services/shared/logger.py
+++ services/shared/logger.py
@@ -8,8 +8,13 @@ def setup_logger(name: str):
     handler = logging.StreamHandler()
     handler.setLevel(logging.INFO)
 
+    file_handler = logging.FileHandler('logs/logs.log')
+    file_handler.setLevel(logging.WARNING)
+
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
 
     logger.addHandler(handler)
+    logger.addHandler(file_handler)
     return logger
